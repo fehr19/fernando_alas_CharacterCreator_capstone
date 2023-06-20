07bb088e2bfc62887e5210caf38ac0180f84bd54
Fernando Alas <fehr19@gmail.com>
2023-06-20 18:32:50 -0500

    Started adding thymeleaf to templates

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,31 +1,37 @@
-const abilityPoints = document.querySelector('.abilityPoints')
+var abilityPoints = 13;
 
 const numberInput = document.querySelector('.number');
 const increaseButton = document.querySelector('.increase');
 const decreaseButton = document.querySelector('.decrease');
 
 increaseButton.addEventListener('click', () => {
-  abilityPoints.value--;
+  abilityPoints--;
   numberInput.value++;
+  document.getElementById("abilityPoints").innerHTML = abilityPoints;
 });
 
 decreaseButton.addEventListener('click', () => {
-  abilityPoints.value++;
+  abilityPoints++;
   numberInput.value--;
+  document.getElementById("abilityPoints").innerHTML = abilityPoints;
 });
+
+
 
 const numberInput1 = document.querySelector('.number1');
 const increaseButton1 = document.querySelector('.increase1');
 const decreaseButton1 = document.querySelector('.decrease1');
 
 increaseButton1.addEventListener('click', () => {
-  abilityPoints.value--;
   numberInput1.value++;
+  abilityPoints--;
+  document.getElementById("abilityPoints").innerHTML = abilityPoints;
 });
 
 decreaseButton1.addEventListener('click', () => {
-  abilityPoints.value++;
   numberInput1.value--;
+  abilityPoints++;
+  document.getElementById("abilityPoints").innerHTML = abilityPoints;
 });
 
 /* Need to write functions for the Abilities increase/decrease buttons */
